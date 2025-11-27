c92678efd1ad55936d3817d5e6616db0c8e9e770
Kagami <shimura.kyohei7617@gmail.com>
2025-11-27 21:23:46 +0900

    chore: README更新に合わせてファイル配置変更
    READMEを最新化
    - 配布手順の最新化
    - 誤ったメッセージの削除
    
    install.shの配置変更
    - cloneしてすぐ使えることを意識

--- install.sh
+++ install.sh
@@ -4,14 +4,27 @@ set -euo pipefail
 REPO="kagami-tsukimura/go-armory"
 VERSION="${1:-latest}"
 
+BIN_DIR="/usr/local/bin"
+
+# 書き込み可能かどうか判定
+if [ -w "$BIN_DIR" ]; then
+  SUDO=""
+else
+  SUDO="sudo"
+fi
+
 install() {
   cmd=$1
   base="https://github.com/$REPO/releases/download/$VERSION"
   file="${cmd}-$(uname | tr '[:upper:]' '[:lower:]')-amd64"
+  url="$base/$file"
+  dest="$BIN_DIR/$cmd"
 
   echo "Downloading $cmd ..."
-  curl -L -o /usr/local/bin/$cmd "$base/$file"
-  chmod +x /usr/local/bin/$cmd
+
+  # sudo いる/いらないを自動切替
+  $SUDO curl -L -o "$dest" "$url"
+  $SUDO chmod +x "$dest"
 }
 
 if [ $# -eq 0 ]; then

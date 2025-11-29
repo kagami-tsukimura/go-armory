747eb54b46d209b9f792bb22d61c00b63747a9db
Kagami <shimura.kyohei7617@gmail.com>
2025-11-29 18:19:07 +0900

    feat: git cloneコマンド
    - clone
    git clone <repository> [directory]

---
+++ internal/clone/clone.go
@@ -0,0 +1,21 @@
+// Package clone provides the core logic for the "clone" CLI command.
+package clone
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/kagami-tsukimura/go-armory/internal"
+)
+
+func Run() {
+	// os.Args[0]: command, os.Args[1]: repository, os.Args[2]: directory
+	if len(os.Args) < 2 || len(os.Args) > 3 {
+		err := fmt.Errorf("requires 1 or 2 arguments\nusage: %s <repository> [directory]", os.Args[0])
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	// slice in parts
+	cmdArgs := append([]string{"clone"}, os.Args[1:]...)
+	internal.RunCmd("git", cmdArgs...)
+}
